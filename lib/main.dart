e738c5ca71c8166e9539512fd3a59ea41aaddc27
Stephboss9 <mbengastephane34@gmail.com>
2022-04-10 02:41:03 -0400

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -39,32 +39,7 @@ class main_page extends StatefulWidget {
 }
 
 class _main_pageState extends State<main_page> {
-  bool isLoading = false;
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  final food_items = <String>[];
-  final food_database = <String>["pizza", "hamburger", "chicken", "quesadilla"];
-
-  String food = "";
-
-  Future<List<String>> extractData() async {
-    // getting response from the targeted url
-    final response = await http.Client().get(Uri.parse(
-        'https://nutrition.umd.edu/shortmenu.aspx?sName=%3cfont+style%3d%22color%3aRed%22%3eDining+%40+Maryland%3c%2ffont%3e&locationNum=04&locationName=%3cfont+style%3d%22color%3aRed%22%3eThe+Diner%3c%2ffont%3e&naFlag=1'));
-    if (response.statusCode == 200) {
-      var document = parser.parse(response.body);
-
-      try {
-        var food_title = document.getElementsByClassName('shortmenumeals')[0];
-        print(food_title);
-
-        return [food_title.text.trim()];
-      } catch (e) {
-        return ['', '', 'ERROR!'];
-      }
-    } else {
-      return ['', '', 'ERROR: ${response.statusCode}.'];
-    }
-  }
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -75,32 +50,10 @@ class _main_pageState extends State<main_page> {
         child: Center(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+          children: []
             // if isLoading is true show loader
             // else show Column of Texts
-            isLoading
-                ? CircularProgressIndicator()
-                : Row(
-                    children: [
-                      MaterialButton(
-                        onPressed: () {
-                          ListView.builder(
-                            padding: const EdgeInsets.all(16.0),
-                            itemBuilder: /*1*/ (context, i) {
-                              if (i.isOdd) return const Divider(); /*2*/
-
-                              final index = i ~/ 2; /*3*/
-                              if (index >= food_items.length) {
-                                food_items.addAll(food_database.take(3)); /*4*/
-                              }
-                              return ListTile(
-                                title: Text(
-                                  food_items[0],
-                                  style: _biggerFont,
-                                ),
-                              );
-                            },
-                          );
+            
                         },
                         child: Text(
                           '250 Diner',
@@ -108,12 +61,10 @@ class _main_pageState extends State<main_page> {
                         ),
                         color: Colors.blue,
                       )
-                    ],
+                    
                   ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.08),
-          ],
-        )),
-      ),
-    );
+          
+        )
+      );
   }
 }
